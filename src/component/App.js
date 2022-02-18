fdee8051e13301990f4a8e89c5997551242feeba
artisteer2020 <99843827+artisteer2020@users.noreply.github.com>
2022-02-18 21:03:26 +0200

    update

--- src/component/App.js
+++ src/component/App.js
@@ -4,17 +4,27 @@ const App = () => {
   const styleP1 = { color: "green", marginTop: "150px" };
   const styleP2 = { color: "red", marginTop: "150px" };
 
-  const myNmae = "hoda";
+  const myNames = "dodo";
   const names = ["adem", "mona", "ali"];
   return (
+    //  log out data from var  line 17
+    // can replace fragment with <> </>
     <Fragment>
-      {myNmae === "ali" ? (
-        <h1 style={styleP1}>{myNmae}</h1>
-      ) : (
-        <h1 style={styleP2}>{myNmae}</h1>
-      )}
-      ; names:
+      {/*  log out data from var  line  */}
+      <p>
+        {" "}
+        the var data is :{" "}
+        {myNames === "hoda" ? (
+          // use ternamry Operator
+          // use in line style
+          <p style={styleP1}> yes the name of var is {myNames} </p>
+        ) : (
+          <p style={styleP2}> yes the name of var is {myNames} </p>
+        )}{" "}
+      </p>
       <div>
+        {/* use map not use foreach to display data from arr */}
+        The Names Of Arr Are :{" "}
         {names.map((el) => (
           <p> {el} </p>
         ))}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,4 @@
 
-
-
-
-
-
-
 import { StrictMode } from "react";
 import ReactDOM from "react-dom";
 
