0595a1c211a0f1390d07c500345c6da36866ca3f
artisteer2020 <99843827+artisteer2020@users.noreply.github.com>
2022-02-20 08:46:53 +0200

    change 1

--- src/component/App/App2.js
+++ src/component/App/App2.js
@@ -1,9 +1,12 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import Cards from "../Cards/Cards";
 import "./App2.css";
 
 const App2 = () => {
-  const Boys = [
+  // use state need
+  // 1- holding data by  => state
+  // 2- control Data  => set State
+  const [state, setState] = useState([
     {
       name: "mohamed",
       age: 30,
@@ -22,11 +25,9 @@ const App2 = () => {
       address: "eygpt",
       phone: "01242155"
     }
-  ];
+  ]);
 
-  // use new data in new arr (reuseable for componnenr)
-
-  const Girls = [
+  const [state1, setState1] = useState([
     {
       name: "hoda",
       age: 12,
@@ -45,7 +46,50 @@ const App2 = () => {
       address: "eygpt",
       phone: "888888888888"
     }
-  ];
+  ]);
+  // const Boys = [
+  //   {
+  //     name: "mohamed",
+  //     age: 30,
+  //     address: "minia",
+  //     phone: "0110212250"
+  //   },
+  //   {
+  //     name: "ali",
+  //     age: 33,
+  //     address: "giza",
+  //     phone: "65665523"
+  //   },
+  //   {
+  //     name: "hany",
+  //     age: 55,
+  //     address: "eygpt",
+  //     phone: "01242155"
+  //   }
+  // ];
+
+  // use new data in new arr (reuseable for componnenr)
+
+  // const Girls = [
+  //   {
+  //     name: "hoda",
+  //     age: 12,
+  //     address: "cairo",
+  //     phone: "021222111222"
+  //   },
+  //   {
+  //     name: "asma",
+  //     age: 10,
+  //     address: "france",
+  //     phone: "65665523"
+  //   },
+  //   {
+  //     name: "dlal",
+  //     age: 55,
+  //     address: "eygpt",
+  //     phone: "888888888888"
+  //   }
+  // ];
   // Each child in a list shoud have a unique "key" prop
   // نحتاجه في الدوم الوهمي لتتبع الدفات والعاناصر
 
@@ -53,10 +97,18 @@ const App2 = () => {
     <Fragment>
       <div className="maincontainer">
         <h1> Boyes Data ... </h1>
-        <Cards ArrData={Boys} type="boys" />
+        <Cards ArrData={state} type="boys" />
 
+        {/* // use arr Girls that holding Data */}
         <h1> Girls Data ... </h1>
-        <Cards ArrData={Girls} type="girls" />
+        <Cards ArrData={state1} type="girls" />
+        {/* 
+        // use arr Boys Arr that holding Data
+         <h1> Boyes Data ... </h1>
+        <Cards ArrData={Boys} type="boys" />
+        // use arr Girls Arr that holding Data
+        <h1> Girls Data ... </h1>
+        <Cards ArrData={Girls} type="girls" /> */}
       </div>
     </Fragment>
   );
